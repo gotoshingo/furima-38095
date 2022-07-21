d505ec6831b4a27a313fed36a44e97f80dcaf82d
gotoshingo <n7es38kscu1mudb5aad@icloud.com>
2022-07-21 18:48:20 +0900

    テストコード

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,47 +2,47 @@ require 'rails_helper'
  RSpec.describe User, type: :model do
    describe "ユーザー新規登録" do
      it "nick_nameが空だと登録できない" do
-       user = User.new(nick_name: "", email: "kkk@gmail.com", encrypted_password: "abc123", password_confirmation: "abc123",first_name: "太郎",second_name: "田中",first_name_kana: "タロウ",second_name_kana: "タナカ",date_of_birth: "2001", "12" , "11")
+       user = User.new(nick_name: "", email: "kkk@gmail.com", encrypted_password: "abc123", password_confirmation: "abc123",first_name: "太郎",second_name: "田中",first_name_kana: "タロウ",second_name_kana: "タナカ",date_of_birth: "2001-12-11")
        user.valid?
        expect(user.errors.full_messages).to include("nick_name can't be blank")
      end
      it "emailが空では登録できない" do
-       user = User.new(nick_name: "abe", email: "", encrypted_password: "abc123", password_confirmation: "abc123",first_name: "太郎",second_name: "田中",first_name_kana: "タロウ",second_name_kana: "タナカ",date_of_birth: "2001", "12" , "11")
+       user = User.new(nick_name: "abe", email: "", encrypted_password: "abc123", password_confirmation: "abc123",first_name: "太郎",second_name: "田中",first_name_kana: "タロウ",second_name_kana: "タナカ",date_of_birth: "2001-12-11")
        user.valid?
        expect(user.errors.full_messages).to include("Email can't be blank")
      end
      it "encrypted_passwordが空では登録できない" do
-      user = User.new(nick_name: "abe", email: "kk@gmail.com", encrypted_password: "", password_confirmation: "abc123",first_name: "太郎",second_name: "田中",first_name_kana: "タロウ",second_name_kana: "タナカ",date_of_birth: "2001", "12" , "11")
+      user = User.new(nick_name: "abe", email: "kk@gmail.com", encrypted_password: "", password_confirmation: "abc123",first_name: "太郎",second_name: "田中",first_name_kana: "タロウ",second_name_kana: "タナカ",date_of_birth: "2001-12-11")
       user.valid?
       expect(user.errors.full_messages).to include("encrypted_password can't be blank")
     end
     it "password_confirmationがencrypted_passwordと一致でないと登録できない" do
-      user = User.new(nick_name: "abe", email: "kk@gmail.com", encrypted_password: "abc123", password_confirmation: "123abc",first_name: "太郎",second_name: "田中",first_name_kana: "タロウ",second_name_kana: "タナカ",date_of_birth: "2001", "12" , "11")
+      user = User.new(nick_name: "abe", email: "kk@gmail.com", encrypted_password: "abc123", password_confirmation: "123abc",first_name: "太郎",second_name: "田中",first_name_kana: "タロウ",second_name_kana: "タナカ",date_of_birth: "2001-12-11")
       user.valid?
       expect(user.errors.full_messages).to include("password_confirmation can't be blank")
     end
     it "password_confirmationが空では登録できない" do
-      user = User.new(nick_name: "abe", email: "kk@gmail.com", encrypted_password: "abc123", password_confirmation: "",first_name: "太郎",second_name: "田中",first_name_kana: "タロウ",second_name_kana: "タナカ",date_of_birth: "2001", "12" , "11")
+      user = User.new(nick_name: "abe", email: "kk@gmail.com", encrypted_password: "abc123", password_confirmation: "",first_name: "太郎",second_name: "田中",first_name_kana: "タロウ",second_name_kana: "タナカ",date_of_birth: "2001-12-11")
       user.valid?
       expect(user.errors.full_messages).to include("encrypted_password can't be blank")
     end
     it "first_nameが空では登録できない" do
-      user = User.new(nick_name: "abe", email: "kk@gmail.com", encrypted_password: "abc123", password_confirmation: "abc123",first_name: "",second_name: "田中",first_name_kana: "タロウ",second_name_kana: "タナカ",date_of_birth: "2001", "12" , "11")
+      user = User.new(nick_name: "abe", email: "kk@gmail.com", encrypted_password: "abc123", password_confirmation: "abc123",first_name: "",second_name: "田中",first_name_kana: "タロウ",second_name_kana: "タナカ",date_of_birth: "2001-12-11")
       user.valid?
       expect(user.errors.full_messages).to include("first_name can't be blank")
     end
     it "second_nameが空では登録できない" do
-      user = User.new(nick_name: "abe", email: "kk@gmail.com", encrypted_password: "abc123", password_confirmation: "abc123",first_name: "太郎",second_name: "",first_name_kana: "タロウ",second_name_kana: "タナカ",date_of_birth: "2001", "12" , "11")
+      user = User.new(nick_name: "abe", email: "kk@gmail.com", encrypted_password: "abc123", password_confirmation: "abc123",first_name: "太郎",second_name: "",first_name_kana: "タロウ",second_name_kana: "タナカ",date_of_birth: "2001-12-11")
       user.valid?
       expect(user.errors.full_messages).to include("second_name can't be blank")
     end
     it "first_name_kanaが空では登録できない" do
-      user = User.new(nick_name: "abe", email: "kk@gmail.com", encrypted_password: "abc123", password_confirmation: "abc123",first_name: "太郎",second_name: "田中",first_name_kana: "",second_name_kana: "タナカ",date_of_birth: "2001", "12" , "11")
+      user = User.new(nick_name: "abe", email: "kk@gmail.com", encrypted_password: "abc123", password_confirmation: "abc123",first_name: "太郎",second_name: "田中",first_name_kana: "",second_name_kana: "タナカ",date_of_birth: "2001-12-11")
       user.valid?
       expect(user.errors.full_messages).to include("first_name_kana can't be blank")
     end
     it "second_name_kanaが空では登録できない" do
-      user = User.new(nick_name: "abe", email: "kk@gmail.com", encrypted_password: "abc123", password_confirmation: "abc123",first_name: "太郎",second_name: "田中",first_name_kana: "タロウ",second_name_kana: "",date_of_birth: "2001", "12" , "11")
+      user = User.new(nick_name: "abe", email: "kk@gmail.com", encrypted_password: "abc123", password_confirmation: "abc123",first_name: "太郎",second_name: "田中",first_name_kana: "タロウ",second_name_kana: "",date_of_birth: "2001-12-11")
       user.valid?
       expect(user.errors.full_messages).to include("second_name_kana can't be blank")
     end
