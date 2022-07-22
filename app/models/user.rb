c625117aaf274be1eb10ebf61ffe16d30a30d69b
gotoshingo <n7es38kscu1mudb5aad@icloud.com>
2022-07-22 18:28:30 +0900

    バリデーションの修正とテストコードの更なる追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :items
-  has_many :buys
+  #has_many :items
+  #has_many :buys
 
-  validates :first_name, presence: true
-  validates :second_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :second_name_kana, presence: true
+
+
   validates :nick_name, presence: true
   validates :date_of_birth, presence: true
+  validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
 
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do  
+    validates :first_name
+    validates :second_name
+  end
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+    validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :second_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,13 +11,43 @@ require 'rails_helper'
        user.valid?
        expect(user.errors.full_messages).to include("Email can't be blank")
      end
-     it "encrypted_passwordが空では登録できない" do
+     it "重複したメールアドレスは登録できない" do
+      user = User.new(nick_name: "abe", email: "test@test", encrypted_password: "abc123", password_confirmation: "abc123",first_name: "太郎",second_name: "田中",first_name_kana: "タロウ",second_name_kana: "タナカ",date_of_birth: "2001-12-11")
+      user.valid?
+      expect(user.errors.full_messages).to include("Password is invalid")
+    end
+    it "メールアドレスに@を含まない場合は登録できない" do
+      user = User.new(nick_name: "abe", email: "kkkgmail.com", encrypted_password: "abc123", password_confirmation: "abc123",first_name: "太郎",second_name: "田中",first_name_kana: "タロウ",second_name_kana: "タナカ",date_of_birth: "2001-12-11")
+      user.valid?
+      expect(user.errors.full_messages).to include("Email is invalid")
+    end
+    it "encrypted_passwordが空では登録できない" do
       user = User.new(nick_name: "abe", email: "kk@gmail.com", encrypted_password: "", password_confirmation: "abc123",first_name: "太郎",second_name: "田中",first_name_kana: "タロウ",second_name_kana: "タナカ",date_of_birth: "2001-12-11")
       user.valid?
       expect(user.errors.full_messages).to include("Password can't be blank")
     end
     it "password_confirmationがencrypted_passwordと一致でないと登録できない" do
       user = User.new(nick_name: "abe", email: "kk@gmail.com", encrypted_password: "abc123", password_confirmation: "123abc",first_name: "太郎",second_name: "田中",first_name_kana: "タロウ",second_name_kana: "タナカ",date_of_birth: "2001-12-11")
+      user.valid?
+      expect(user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+    it "パスワードが6文字未満では登録できない" do
+      user = User.new(nick_name: "abe", email: "kk@gmail.com", encrypted_password: "abc12", password_confirmation: "abc12",first_name: "太郎",second_name: "田中",first_name_kana: "タロウ",second_name_kana: "タナカ",date_of_birth: "2001-12-11")
+      user.valid?
+      expect(user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+    it "英字のみのパスワードでは登録できない" do
+      user = User.new(nick_name: "abe", email: "kk@gmail.com", encrypted_password: "abcdef", password_confirmation: "abcdef",first_name: "太郎",second_name: "田中",first_name_kana: "タロウ",second_name_kana: "タナカ",date_of_birth: "2001-12-11")
+      user.valid?
+      expect(user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+    it "数字のみのパスワードでは登録できない" do
+      user = User.new(nick_name: "abe", email: "kk@gmail.com", encrypted_password: "123456", password_confirmation: "123456",first_name: "太郎",second_name: "田中",first_name_kana: "タロウ",second_name_kana: "タナカ",date_of_birth: "2001-12-11")
+      user.valid?
+      expect(user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+    it "全角文字を含むパスワードでは登録できない" do
+      user = User.new(nick_name: "abe", email: "kk@gmail.com", encrypted_password: "あbc1２3", password_confirmation: "あbc1２3",first_name: "太郎",second_name: "田中",first_name_kana: "タロウ",second_name_kana: "タナカ",date_of_birth: "2001-12-11")
       user.valid?
       expect(user.errors.full_messages).to include("Password confirmation doesn't match Password")
     end
@@ -31,20 +61,40 @@ require 'rails_helper'
       user.valid?
       expect(user.errors.full_messages).to include("First name can't be blank")
     end
+    it "姓（全角）に半角文字が含まれていると登録できない" do
+      user = User.new(nick_name: "abe", email: "kk@gmail.com", encrypted_password: "abc123", password_confirmation: "abc123",first_name: "t太郎",second_name: "田中",first_name_kana: "タロウ",second_name_kana: "タナカ",date_of_birth: "2001-12-11")
+      user.valid?
+      expect(user.errors.full_messages).to include("First name is invalid")
+    end
     it "second_nameが空では登録できない" do
       user = User.new(nick_name: "abe", email: "kk@gmail.com", encrypted_password: "abc123", password_confirmation: "abc123",first_name: "太郎",second_name: "",first_name_kana: "タロウ",second_name_kana: "タナカ",date_of_birth: "2001-12-11")
       user.valid?
       expect(user.errors.full_messages).to include("Second name can't be blank")
+    end
+    it "名（全角）に半角文字が含まれていると登録できない" do
+      user = User.new(nick_name: "abe", email: "kk@gmail.com", encrypted_password: "abc123", password_confirmation: "abc123",first_name: "太郎",second_name: "t田中",first_name_kana: "タロウ",second_name_kana: "タナカ",date_of_birth: "2001-12-11")
+      user.valid?
+      expect(user.errors.full_messages).to include("Second name is invalid")
     end
     it "first_name_kanaが空では登録できない" do
       user = User.new(nick_name: "abe", email: "kk@gmail.com", encrypted_password: "abc123", password_confirmation: "abc123",first_name: "太郎",second_name: "田中",first_name_kana: "",second_name_kana: "タナカ",date_of_birth: "2001-12-11")
       user.valid?
       expect(user.errors.full_messages).to include("First name kana can't be blank")
     end
+    it "姓（カナ）にカタカナ以外の文字（平仮名・漢字・英数字・記号）が含まれていると登録できない" do
+      user = User.new(nick_name: "abe", email: "kk@gmail.com", encrypted_password: "abc123", password_confirmation: "abc123",first_name: "太郎",second_name: "田中",first_name_kana: "太ろu+",second_name_kana: "タナカ",date_of_birth: "2001-12-11")
+      user.valid?
+      expect(user.errors.full_messages).to include("First name kana is invalid")
+    end
     it "second_name_kanaが空では登録できない" do
       user = User.new(nick_name: "abe", email: "kk@gmail.com", encrypted_password: "abc123", password_confirmation: "abc123",first_name: "太郎",second_name: "田中",first_name_kana: "タロウ",second_name_kana: "",date_of_birth: "2001-12-11")
       user.valid?
       expect(user.errors.full_messages).to include("Second name kana can't be blank")
+    end
+    it "名（カナ）にカタカナ以外の文字（平仮名・漢字・英数字・記号）が含まれていると登録できない" do
+      user = User.new(nick_name: "abe", email: "kk@gmail.com", encrypted_password: "abc123", password_confirmation: "abc123",first_name: "太郎",second_name: "田中",first_name_kana: "タロウ",second_name_kana: "田なka?",date_of_birth: "2001-12-11")
+      user.valid?
+      expect(user.errors.full_messages).to include("Second name kana is invalid")
     end
     it "date_of_birthが空では登録できない" do
       user = User.new(nick_name: "abe", email: "kk@gmail.com", encrypted_password: "abc123", password_confirmation: "abc123",first_name: "太郎",second_name: "田中",first_name_kana: "タロウ",second_name_kana: "タナカ",date_of_birth: "")
