33cf815e1a8821b9aa6e756f665e1615ddc81a04
gotoshingo <n7es38kscu1mudb5aad@icloud.com>
2022-08-02 16:43:06 +0900

    その他の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_furima, only: [:edit, :show, :update, :destroy]
+  # before_action :set_furima, only: [:edit, :show, :update, :destroy]
 
   def index
     #@items = item.all
@@ -8,52 +8,52 @@ class ItemsController < ApplicationController
   end
 
   def new
-    @item = Item.new
+    @items = Item.new
   end
 
   def create
-    @item = Item.new(item_params)
-    if @item.save
+    @items = Item.new(items_params)
+    if @items.save
       redirect_to root_path
     else
       render :new
     end
   end
 
-  def show
-  end
+  # def show
+  # end
 
-  def edit
-    if Record.exists?(items_id: @items.id) || current_user.id != @items.user_id
-      redirect_to root_path      
-    end
-  end
+  # def edit
+  #   if Record.exists?(items_id: @item.id) || current_user.id != @item.user_id
+  #     redirect_to root_path      
+  #   end
+  # end
 
-  def update
-    if @items.update(items_params)
-      redirect_to items_path(@items)
-    else
-      render :edit
-    end
-  end
+  # def update
+  #   if @item.update(item_params)
+  #     redirect_to item_path(@item)
+  #   else
+  #     render :edit
+  #   end
+  # end
 
-  def destroy
-    if  current_user.id == @items.user_id
-      if @items.destroy
-        redirect_to root_path
-      else
-        redirect_to items_path(@items)
-      end
-    else
-      redirect_to items_path(@items)
-    end
-  end  
+  # def destroy
+  #   if  current_user.id == @item.user_id
+  #     if @item.destroy
+  #       redirect_to root_path
+  #     else
+  #       redirect_to items_path(@item)
+  #     end
+  #   else
+  #     redirect_to items_path(@item)
+  #   end
+  # end  
 
   private
   def items_params
     params.require(:item).permit(:image, :item_name, :item_category_id, :item_condition_id, :item_address_id, :item_explanation, :delivery_category_id, :delivery_dey_id, :price).merge(user_id: current_user.id)
   end
-  def set_items
-    @items =Items.find(params[:id])
-  end
+  # def set_item
+  #   @item =Item.find(params[:id])
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  resource :items do
+  root to: "items#index"
+  resources :items do
     resources :records, only: [:index, :create]
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "items#index"
 end
