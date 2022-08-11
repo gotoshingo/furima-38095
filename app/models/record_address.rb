777197333c246e8febe525819d0ebf835126c31d
gotoshingo <n7es38kscu1mudb5aad@icloud.com>
2022-08-11 18:22:38 +0900

    修正

--- app/models/record_address.rb
+++ app/models/record_address.rb
@@ -6,6 +6,7 @@ class RecordAddress
     validates :user_id
     validates :item_id
     validates :address_number, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :address_house
     validates :address_city
     validates :phone_number,format: {with: /\A[0-9]{10,11}\z/}
     validates :token
